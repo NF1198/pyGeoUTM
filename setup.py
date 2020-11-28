3201945bb4271a7a9038347b895fa5da442d61f9
Nicholas Folse <nickfolse@gmail.com>
2020-11-27 20:47:21 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Universal Transverse Mercator (UTM) to Lat-Lon Converter",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nf1198",
+    url="https://github.com/NF1198/pyGeoUTM",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
